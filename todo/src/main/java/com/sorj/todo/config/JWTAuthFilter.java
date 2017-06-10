f15c57f8538981c26eaeed7912ba1cfeec006f3c
D061640 <D061640@MUCN34064666A.emea.global.corp.sap>
2017-06-10 18:30:38 +0200

    Validate Token on Request

--- todo/src/main/java/com/sorj/todo/config/JWTAuthFilter.java
+++ todo/src/main/java/com/sorj/todo/config/JWTAuthFilter.java
@@ -1,27 +1,56 @@
 package com.sorj.todo.config;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-public class JWTAuthFilter extends GenericFilterBean {
+import io.jsonwebtoken.Jwts;
+
+public class JWTAuthFilter extends OncePerRequestFilter {
 
 	private final Logger log = LoggerFactory.getLogger(JWTAuthFilter.class);
 
 	@Override
-	public void doFilter(ServletRequest req, ServletResponse res, FilterChain filterChain)
-			throws IOException, ServletException {
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
 
-		log.info("Check Token");
+		String token = request.getHeader("Authentication");
 
-		filterChain.doFilter(req, res);
+		log.info("Found Token: " + token);
+
+		try {
+
+			String userName = Jwts.parser().setSigningKey("secret").parseClaimsJws(token).getBody().getSubject();
+
+			log.info("User Name: " + userName);
+
+			List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+			grantedAuthorities.add(new SimpleGrantedAuthority("USER"));
+
+			Authentication auth = new UsernamePasswordAuthenticationToken(userName, null, grantedAuthorities);
+			SecurityContextHolder.getContext().setAuthentication(auth);
+
+			filterChain.doFilter(request, response);
+
+		} catch (Exception e) {
+			throw new SecurityException();
+
+		}
+
 	}
 
 }
--- todo/src/main/java/com/sorj/todo/controller/ToDoController.java
+++ todo/src/main/java/com/sorj/todo/controller/ToDoController.java
@@ -1,19 +1,45 @@
 package com.sorj.todo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sorj.todo.config.JWTLoginFilter;
+import com.sorj.todo.model.ToDo;
+import com.sorj.todo.model.User;
 
 @RestController
+@RequestMapping("/todo/api/v1")
 public class ToDoController {
-	
+
 	private final Logger log = LoggerFactory.getLogger(JWTLoginFilter.class);
-	
-	@GetMapping(path="/")
-	public void root(){
+
+	@GetMapping(path = "/todos")
+	public ResponseEntity<List<ToDo>> listAllToDosOfUser() {
+
+		String userName = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		log.info("User Name in Controller: " + userName);
+
+		User user = new User();
+		user.setFirstName("Steve");
+
+		ToDo todo = new ToDo();
+		todo.setCreatedBy(user);
+		todo.setTodo("Test ToDo Text");
+		todo.setId(new Long(1));
+
+		List<ToDo> todos = new ArrayList<>();
+		todos.add(todo);
+
+		return new ResponseEntity<List<ToDo>>(todos, HttpStatus.OK);
 	}
 
 }
